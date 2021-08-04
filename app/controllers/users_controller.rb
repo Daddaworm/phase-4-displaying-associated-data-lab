1442b92873ba41278847f1c5bd033a3685f05359
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-08-04 13:54:32 -0400

    commit tree 23d8e3184b032d4d100d66f35ecb38faadc31ea4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    # GET /users/:id
+    def show
+        # byebug
+        user = find_user
+        render json: user, include: :items
+    end
+
+
+
+    private
+
+    def find_user
+        user = User.find_by(params[:id])
+    end
+
+    def render_not_found_response
+        render json: { error: "User not found" }, status: :not_found
+    end
 end
